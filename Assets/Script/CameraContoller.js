0235c1d2804b3ec5552fe25e8e324676441c7899
hadayan0 <fumano0@gmail.com>
2016-05-27 19:24:47 +0900

    move camra, player

---
+++ Assets/Script/CameraContoller.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+var player : Transform;
+var rotate_spd : float;
+var appro_spd  : float;
+
+function Start () {
+
+}
+
+function Update () {
+    var h = Input.GetAxis("CamHorizontal") * rotate_spd * Time.deltaTime;
+    var v = Input.GetAxis("CamVertical")   * rotate_spd * Time.deltaTime;
+    var a = Input.GetAxis("CamApproach")   * appro_spd;
+    var angv = transform.eulerAngles.x;
+    Debug.Log("angv="+angv+", v="+v);
+
+    var v_axis = transform.position - player.position;
+    v_axis = Vector3(-v_axis.z, 0, v_axis.x);
+
+
+    if ( angv + v > 90 ) {
+    	v = 90 - angv;
+    }
+    if ( angv + v < 0) {
+    	v = -angv;
+    }
+
+    //Debug.Log("H="+h+", V="+v+", A="+a);
+    transform.RotateAround( player.position, Vector3.up, h );
+    transform.RotateAround( player.position, v_axis, v );
+}
---
+++ Assets/Script/GameController.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+var rotate_spd = 1;
+var appro_spd  = 1;
+
+function Start () {
+
+}
+
+function Update () {
+}
