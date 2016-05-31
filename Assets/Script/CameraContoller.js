9199a60e9b4e5dcef0f0b0b1bd02173851d7a27d
hadayan0 <fumano0@gmail.com>
2016-06-01 03:55:49 +0900

    camera controll complete

--- Assets/Script/CameraContoller.js
+++ Assets/Script/CameraContoller.js
@@ -2,31 +2,47 @@
 
 var player : Transform;
 var rotate_spd : float;
-var appro_spd  : float;
+var vertical_spd : float;
+var approach_spd  : float;
+
+var vertical_max : float;
+var vertical_min : float;
+var approach_max : float;
+var approach_min : float;
 
 function Start () {
 
 }
 
 function Update () {
-    var h = Input.GetAxis("CamHorizontal") * rotate_spd * Time.deltaTime;
-    var v = Input.GetAxis("CamVertical")   * rotate_spd * Time.deltaTime;
-    var a = Input.GetAxis("CamApproach")   * appro_spd;
+    var h =  Input.GetAxis("CamHorizontal") * rotate_spd * Time.deltaTime;
+    var v = -Input.GetAxis("CamVertical")   * vertical_spd * Time.deltaTime;
+    var a = -Input.GetAxis("CamApproach")   * approach_spd * Time.deltaTime;
     var angv = transform.eulerAngles.x;
-    Debug.Log("angv="+angv+", v="+v);
+    //Debug.Log("angv="+angv+", v="+v);
 
+    // vertical
+    //transform.position.y += v;
     var v_axis = transform.position - player.position;
     v_axis = Vector3(-v_axis.z, 0, v_axis.x);
-
-
-    if ( angv + v > 90 ) {
-    	v = 90 - angv;
+    if ( angv + v > vertical_max ) {
+    	v = vertical_max - angv;
     }
-    if ( angv + v < 0) {
-    	v = -angv;
+    else if ( angv + v < vertical_min) {
+    	v = vertical_min - angv;
     }
+    transform.RotateAround( player.position, v_axis, v );
+
+	// horizontal
+    transform.RotateAround( player.position, Vector3.up, h );
+
+    // approach
+    var distance = (transform.position-player.position).magnitude;
+    a = Mathf.Min( a, distance - approach_min );
+	a = Mathf.Max( a, distance - approach_max );
+    transform.Translate( 0, 0, a );
+
+    transform.LookAt( player );
 
     //Debug.Log("H="+h+", V="+v+", A="+a);
-    transform.RotateAround( player.position, Vector3.up, h );
-    transform.RotateAround( player.position, v_axis, v );
 }
--- Assets/Script/GameController.js
+++ Assets/Script/GameController.js
@@ -1,8 +1,5 @@
 ﻿#pragma strict
 
-var rotate_spd = 1;
-var appro_spd  = 1;
-
 function Start () {
 
 }
